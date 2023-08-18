437a760f8ff5fe20077182d83da761adc8968154
Lokesh Dangi <lokeshdangi1045@gmail.com>
2023-08-18 22:04:33 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+
+		    for(int i=0; i<res.size(); i++){
+		        ot.print(res.get(i)+" ");
+		    }
+		    
+		    ot.println();
+		}
+		ot.close();
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        
+        // Creating arraylist to store the leader elements
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int curr_leader = arr[n-1];
+        result.add(curr_leader);
+        
+        // loop to add element from right side of the array
+        for(int i=n-2; i>=0;i--){
+            if(curr_leader<=arr[i]){
+                curr_leader = arr[i];
+                result.add(curr_leader);  // adding leaders in separate ArrayList result
+            }
+        }
+        
+        // reversing the result
+        Collections.reverse(result);
+        
+        return result;
+    }
+}
