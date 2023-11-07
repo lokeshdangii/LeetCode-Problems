cbc52256756e840e4dae56e8805dc1350ddb37d5
Lokesh Dangi <lokeshdangi1045@gmail.com>
2023-11-08 00:13:10 +0530

    Time: 2388 ms (5.21%), Space: 60.5 MB (5.21%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.java
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.java
@@ -7,7 +7,8 @@ class Solution {
             time[i] = (double)dist[i]/speed[i];
         }
 
-        Arrays.sort(time);
+        // Arrays.sort(time);
+        insertionSort(time);
 
         for(int i=0;i<n;i++){
             if(time[i] <= i){
@@ -16,5 +17,27 @@ class Solution {
         }
 
         return n;
+    }
+
+
+    // sorting function
+    public static void insertionSort(double arr[]){
+        // array length
+        int n = arr.length;       
+        for(int i=1;i<n;i++){
+            double ele = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j]>ele){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = ele;
+        }
+        // // loop to print array
+        // for(int i=0;i<n;i++){
+        //     System.out.print(arr[i] + " ");
+        // }
     }
 }
