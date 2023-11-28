d44ddb1b82acc32f66e7766a3a58a6e4b46992af
Lokesh Dangi <lokeshdangi1045@gmail.com>
2023-11-29 01:52:09 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (37.05%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int m = accounts.length;
+        int n = accounts[0].length;
+        int maximum = 0;
+        
+        for(int i=0;i<m;i++){
+            int wealth = 0;
+            for(int j=0;j<n;j++){
+                
+                wealth += accounts[i][j];
+                
+                
+            }
+            
+            maximum = Math.max(wealth,maximum);
+        }
+        
+        return maximum;
+    }
+}
