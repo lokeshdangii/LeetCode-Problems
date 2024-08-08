71a84c030a2c9743d748a9b2b37e4341f79b9ae0
Lokesh Dangi <lokeshdangi1045@gmail.com>
2024-08-08 16:11:41 +0530

    Time: 0 ms (100%), Space: 45.6 MB (53.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,8 +4,8 @@ class Solution {
         int profit = 0;
 
         for(int i=1;i<n;i++){
-            if(prices[i]>prices[i-1]){
-                profit += ( prices[i] - prices[i-1] );
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
             }
         }
 
